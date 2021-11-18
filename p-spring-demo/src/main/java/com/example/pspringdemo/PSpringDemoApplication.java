c63dc4a15b1d8d6a3fa859639f9de92fceb5b947
branwn <50729260+branwn@users.noreply.github.com>
2021-11-19 01:41:50 +0800

    trying to using spring

---
+++ p-spring-demo/src/main/java/com/example/pspringdemo/PSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PSpringDemoApplication.class, args);
+	}
+
+}
---
+++ p-spring-demo/src/test/java/com/example/pspringdemo/PSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
